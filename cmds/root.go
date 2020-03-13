ab464dfdd4b14e4b53eeaf4fd8108656727df7e6
kooksee <kooksee@163.com>
2020-03-13 17:03:27 +0800

    fix: update

--- cmds/root.go
+++ cmds/root.go
@@ -1,16 +1,17 @@
 package cmds
 
 import (
-	"github.com/pubgo/g/xcmd"
 	"github.com/pubgo/wizsdk/version"
+	"github.com/pubgo/xcmd/xcmd"
 	"github.com/pubgo/xcmd/xcmd/xcmd_wv"
 )
 
-const Service = "WIZ"
+const Service = "wiz"
 
 // Execute exec
 var Execute = xcmd.Init(func(cmd *xcmd.Command) {
 	cmd.Version = version.Version
+	cmd.Use = Service
 
 	cmd.AddCommand(
 		xcmd_wv.Init(),
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/pubgo/wizsdk
 go 1.14
 
 require (
-	github.com/pubgo/g v0.2.44
-	github.com/pubgo/xcmd v0.0.0-20200306020059-de492ad2acfe
+	github.com/pubgo/g v0.2.44 // indirect
+	github.com/pubgo/xcmd v0.0.2
 )
